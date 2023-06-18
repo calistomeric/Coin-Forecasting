0a7051b0fe722e324d84850040c7854153e55f15
Okonkwo Calistus <99477055+calistomeric@users.noreply.github.com>
2023-06-18 12:43:53 +0100

    Update coin_forecast.py

--- coin_forecast.py
+++ coin_forecast.py
@@ -18,12 +18,17 @@ api = tweepy.API(auth, wait_on_rate_limit=True)
 
 # "mongodb+srv://calistus:calistus@cluster0.m97pu3i.mongodb.net/?retryWrites=true&w=majority"
 # mongo_client = pymongo.MongoClient("mongodb+srv://burlacuemilia07:root@dbdisertatie.syso6he.mongodb.net/?retryrites=true&w=majority")
-mongo_client = pymongo.MongoClient("mongodb+srv://calistus:calistus@cluster0.m97pu3i.mongodb.net/?retryWrites=true&w=majority")
+# mongo_client = pymongo.MongoClient("mongodb+srv://calistus:calistus@cluster0.m97pu3i.mongodb.net/?retryWrites=true&w=majority")
 client = tweepy.Client(bearer_token=Bearer_Token, wait_on_rate_limit=True)
 # db = mongo_client['DBdisertatie']
+# db = mongo_client['StoreTweet']
+
+@st.experimental_singleton(suppress_st_warning=True)
+def init_connection():
+    return MongoClient("mongodb+srv://st.secrets.username:st.secrets.password@st.secrets.clustername.m97pu3i.mongodb.net/?retryWrites=true&w=majority")
+
+mongo_client = init_connection()
 db = mongo_client['StoreTweet']
-
-
 
 def get_tweet(start, end, search_term, limit):
     #####################################################
